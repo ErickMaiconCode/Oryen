5a3dd2fc1521baeae42abb83e50e18f7a7480457
Erick <erick.lima195@outlook.com>
2025-12-15 10:15:32 -0300

    Estrutura inicial de pastas

---
+++ App/Oryen.swift
@@ -0,0 +1,18 @@
+//
+//  Oryen.swift
+//  
+//
+//  Created by Erick Maicon Lima de Almeida on 15/12/25.
+//
+
+import SwiftUI
+
+struct Oryen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Oryen()
+}
