0a387b885f09d5d517699e334233c5e0b8b1cc3e
Erick <erick.lima195@outlook.com>
2025-12-15 13:34:27 -0300

    Initial Commit

---
+++ Oryen/OryenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OryenApp.swift
+//  Oryen
+//
+//  Created by Erick Maicon Lima de Almeida on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct OryenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
